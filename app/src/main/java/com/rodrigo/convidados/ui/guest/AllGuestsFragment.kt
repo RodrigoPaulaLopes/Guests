f1a671287c91c077429bc86699ecf4dc97815c4d
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2024-07-07 11:30:50 -0300

    feat: create alert to delete guest

--- app/src/main/java/com/rodrigo/convidados/ui/guest/AllGuestsFragment.kt
+++ app/src/main/java/com/rodrigo/convidados/ui/guest/AllGuestsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -42,6 +43,7 @@ class AllGuestsFragment : Fragment() {
             }
 
             override fun onDelete(id: Int) {
+
                 viewModel.delete(id)
                 viewModel.getAll()
             }
--- app/src/main/java/com/rodrigo/convidados/ui/viewHolder/GuestViewHolder.kt
+++ app/src/main/java/com/rodrigo/convidados/ui/viewHolder/GuestViewHolder.kt
@@ -2,13 +2,15 @@ package com.rodrigo.convidados.ui.viewHolder
 
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.rodrigo.convidados.R
 import com.rodrigo.convidados.databinding.RowGuestBinding
 import com.rodrigo.convidados.listners.OnGuestListner
 import com.rodrigo.convidados.model.GuestModel
 
-class GuestViewHolder(private val item: RowGuestBinding, private val listner: OnGuestListner) : ViewHolder(item.root) {
+class GuestViewHolder(private val item: RowGuestBinding, private val listner: OnGuestListner) :
+    ViewHolder(item.root) {
 
 
     fun bind(guest: GuestModel) {
@@ -20,7 +22,13 @@ class GuestViewHolder(private val item: RowGuestBinding, private val listner: On
         }
 
         item.txtGuestName.setOnLongClickListener {
-            listner.onDelete(guest.id)
+            AlertDialog.Builder(itemView.context)
+                .setTitle("Remoção de convidado").setMessage("Tem certeza que deseja remover?")
+                .setPositiveButton("Sim") { dialog, which -> listner.onDelete(guest.id)}
+                .setNegativeButton("Não") { dialog, which -> null}
+                .create()
+                .show()
+
             true
         }
     }
